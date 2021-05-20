d1ed8a7313f2c7e406948f44605172d4f8c40471
Zhonghuan Gao <zhonghuangao@gmail.com>
2021-05-20 13:37:29 +0800

    Add golang tcp server example

---
+++ go/client/example_tcp_client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+// Application constants, defining host, port, and protocol.
+const (
+	connHost = "localhost"
+	connPort = "8080"
+	connType = "tcp"
+)
+
+func main() {
+	// Start the client and connect to the server.
+	fmt.Println("Connecting to", connType, "server", connHost+":"+connPort)
+	conn, err := net.Dial(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error connecting:", err.Error())
+		os.Exit(1)
+	}
+
+	// Create new reader from Stdin.
+	reader := bufio.NewReader(os.Stdin)
+
+	// run loop forever, until exit.
+	for {
+		// Prompting message.
+		fmt.Print("Text to send: ")
+
+		// Read in input until newline, Enter key.
+		input, _ := reader.ReadString('\n')
+
+		// Send to socket connection.
+		conn.Write([]byte(input))
+
+		// Listen for relay.
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+
+		// Print server relay.
+		log.Print("Server relay: " + message)
+	}
+}
---
+++ go/server/example_tcp_server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+// Application constants, defining host, port, and protocol.
+const (
+	connHost = "localhost"
+	connPort = "8080"
+	connType = "tcp"
+)
+
+func main() {
+	// Start the server and listen for incoming connections.
+	fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
+	l, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+
+	// run loop forever, until exit.
+	for {
+		// Listen for an incoming connection.
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error connecting:", err.Error())
+			return
+		}
+		fmt.Println("Client connected.")
+
+		// Print client connection address.
+		fmt.Println("Client " + c.RemoteAddr().String() + " connected.")
+
+		// Handle connections concurrently in a new goroutine.
+		go handleConnection(c)
+	}
+}
+
+// handleConnection handles logic for a single connection request.
+func handleConnection(conn net.Conn) {
+	// Buffer client input until a newline.
+	buffer, err := bufio.NewReader(conn).ReadBytes('\n')
+
+	// Close left clients.
+	if err != nil {
+		fmt.Println("Client left.")
+		conn.Close()
+		return
+	}
+
+	// Print response message, stripping newline character.
+	log.Println("Client message:", string(buffer[:len(buffer)-1]))
+
+	// Send response message to the client.
+	conn.Write(buffer)
+
+	// Restart the process.
+	handleConnection(conn)
+}
